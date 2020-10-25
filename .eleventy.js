e1b54261196daf5968dc2ed09a59899c0c0e9dfa
David Carter <dajocarter@gmail.com>
2020-10-25 15:21:16 -0700

    add layout alias for default

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
-module.exports = {
-  dir: {
-    input: "src",
-    output: "dist"
+module.exports = function(eleventyConfig) {
+  // alias for layouts in frontmatter
+  eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
+  // return directory config
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
   }
-};
+}
