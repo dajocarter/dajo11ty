9c18d792f430c39f5329db06b8724cba2b9030dc
David Carter <dajocarter@gmail.com>
2020-10-27 15:37:43 -0700

    create a theme and extend typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,77 @@ module.exports = {
     content: ['./src/**/*.njk']
   },
   theme: {
-    extend: {}
+    extend: {
+      colors: {
+        theme: 'var(--theme--color__text)',
+        link: 'var(--theme--color__link)',
+        background: 'var(--theme--color__background)'
+      },
+      textColor: {
+        theme: 'var(--theme--color__text)',
+        link: 'var(--theme--color__link)'
+      },
+      backgroundColor: {
+        theme: 'var(--theme--color__background)'
+      }
+    },
+    typography: {
+      default: {
+        css: {
+          color: 'var(--theme--color__text)',
+          a: {
+            color: 'var(--theme--color__link)'
+          },
+          button: {
+            color: 'var(--theme--color__link)'
+          },
+          strong: {
+            color: 'var(--theme--color__link)'
+          },
+          hr: {
+            borderColor: 'var(--theme--color__link)'
+          },
+          h1: {
+            color: 'var(--theme--color__text)'
+          },
+          h2: {
+            color: 'var(--theme--color__text)'
+          },
+          h3: {
+            color: 'var(--theme--color__text)'
+          },
+          h4: {
+            color: 'var(--theme--color__text)'
+          },
+          h5: {
+            color: 'var(--theme--color__text)'
+          },
+          h6: {
+            color: 'var(--theme--color__text)'
+          },
+          'ol li:before': {
+            color: 'var(--theme--color__link)'
+          },
+          'ul li:before': {
+            backgroundColor: 'var(--theme--color__link)'
+          },
+          blockquote: {
+            borderLeftColor: 'var(--theme--color__link)',
+            color: 'var(--theme--color__text)'
+          },
+          pre: {
+            color: 'var(--theme--color__background)',
+            backgroundColor: 'var(--theme--color__text)'
+          },
+          code: {
+            color: 'var(--theme--color__link)'
+          },
+          thead: {
+            color: 'var(--theme--color__link)'
+          }
+        }
+      }
+    }
   },
   variants: {},
   plugins: [
