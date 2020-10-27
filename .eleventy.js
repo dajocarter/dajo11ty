0df9f79606571aaf2b5bb5d876a731640389f3bf
David Carter <dajocarter@gmail.com>
2020-10-27 15:35:27 -0700

    create post layout

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   // alias for layouts in frontmatter
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
+  eleventyConfig.addLayoutAlias('post', 'layouts/post.njk')
   // pass through static assets
   eleventyConfig.addPassthroughCopy('src/styles/index.css')
   // return directory config
