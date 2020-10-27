0c7e30f957a61035f619a937af50ef8d0b073ef5
David Carter <dajocarter@gmail.com>
2020-10-27 15:36:35 -0700

    create filter for dates and output post date

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,11 @@
+const { DateTime } = require('luxon')
 module.exports = function(eleventyConfig) {
   // alias for layouts in frontmatter
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk')
+  // functions to transform content
+  eleventyConfig.addFilter('humanDateString', date => DateTime.fromJSDate(date, { zone: 'utc' }).toFormat('LLLL d, yyyy'))
+  eleventyConfig.addFilter('htmlDateString', date => DateTime.fromJSDate(date, { zone: 'utc' }).toFormat('yyyy-LL-dd'))
   // pass through static assets
   eleventyConfig.addPassthroughCopy('src/styles/index.css')
   // return directory config
