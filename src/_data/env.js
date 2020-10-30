820b30ac45b72033fad3cb4eaaf1f2647ed6cc95
David Carter <dajocarter@gmail.com>
2020-10-30 00:03:46 -0700

    update netlify url

--- src/_data/env.js
+++ src/_data/env.js
@@ -1,7 +1,7 @@
 const GOOGLE_ANALYTICS_ID = process.env.GOOGLE_ANALYTICS_ID
 const environment = process.env.ELEVENTY_ENV
 const isProd = environment === 'production'
-const baseURL = isProd ? 'https://dajo11ty.netlify.com' : 'http://localhost:8080'
+const baseURL = isProd ? 'https://dajo11ty.netlify.app' : 'http://localhost:8080'
 
 module.exports = {
   site: {
