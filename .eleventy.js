60cf06a4cded8714611d08f3f23380b510f3740e
David Carter <dajocarter@gmail.com>
2020-11-10 22:10:15 -0800

    enable 404 page locally

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,9 @@
 require('dotenv').config()
 const { DateTime } = require('luxon')
-const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight")
+const fs = require('fs')
+const syntaxHighlight = require('@11ty/eleventy-plugin-syntaxhighlight')
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // alias for layouts in frontmatter
   eleventyConfig.addLayoutAlias('default', 'layouts/default.njk')
   eleventyConfig.addLayoutAlias('post', 'layouts/post.njk')
@@ -16,6 +17,21 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy('src/styles/prism-coy-without-shadows.css')
   eleventyConfig.addPassthroughCopy('src/img')
   eleventyConfig.addPassthroughCopy('src/scripts/index.js')
+  // Enables 404 page locally
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, browserSync) {
+        browserSync.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync('dist/404.html');
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html" });
+          res.end();
+        });
+      }
+    }
+  })
   // return directory config
   return {
     dir: {
